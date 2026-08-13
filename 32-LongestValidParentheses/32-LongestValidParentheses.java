8505f5b148a29cac4384930e0bf73cd636a86c7f
vishnu-28g <vishnuvishnu50097@gmail.com>
2026-08-13 09:23:33 +0530

    Create 32-LongestValidParentheses

---
+++ 32-LongestValidParentheses/32-LongestValidParentheses.java
@@ -0,0 +1,23 @@
+// Last updated: 13/08/2026, 09:23:33
+class Solution {
+    public int longestValidParentheses(String s) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        int maxLength = 0;
+        for(int i =0; i<s.length() ;i++){
+            if(s.charAt(i) == '('){
+                stack.push(i);
+            }
+            else{
+                stack.pop();
+                if(stack.isEmpty()){
+                    stack.push(i);
+                }
+                else{
+                     maxLength = Math.max(maxLength, i - stack.peek());
+                }
+            }
+        }
+        return maxLength;
+    }
+}
