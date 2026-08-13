385d8833c2aacccac9b7708a11e665aaf8fb0698
vishnu-28g <vishnuvishnu50097@gmail.com>
2026-08-13 09:21:32 +0530

    Create 145-BinaryTreePostorderTraversal

---
+++ 145-BinaryTreePostorderTraversal/145-BinaryTreePostorderTraversal.java
@@ -0,0 +1,23 @@
+// Last updated: 13/08/2026, 09:21:31
+
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+	LinkedList<Integer> ans = new LinkedList<>();
+	Stack<TreeNode> stack = new Stack<>();
+	if (root == null) return ans;
+	
+	stack.push(root);
+	while (!stack.isEmpty()) {
+		TreeNode cur = stack.pop();
+		ans.addFirst(cur.val);
+		if (cur.left != null) {
+			stack.push(cur.left);
+		}
+		if (cur.right != null) {
+			stack.push(cur.right);
+		} 
+	}
+	return ans;
+}
+
+}
