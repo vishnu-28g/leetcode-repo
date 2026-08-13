266721920ee67660f5daf4e87e0151dc2590920c
vishnu-28g <vishnuvishnu50097@gmail.com>
2026-08-13 09:13:04 +0530

    Create 1039-FindTheTownJudge

---
+++ 1039-FindTheTownJudge/1039-FindTheTownJudge.java
@@ -0,0 +1,18 @@
+// Last updated: 13/08/2026, 09:13:02
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        int[] count = new int[n+1];
+        for(int[] t : trust){
+            count[t[0]]--;
+            count[t[1]]++;
+        }
+        for(int i =1;i<=n;i++){
+            if(count[i] == n-1){
+                return i;
+            }
+        }
+        return -1;
+        
+    }
+}
+
