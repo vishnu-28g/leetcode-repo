f448038e6150600e62c2e02710075adb63d67401
vishnu-28g <vishnuvishnu50097@gmail.com>
2026-08-13 09:23:56 +0530

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/20-ValidParentheses.java
@@ -0,0 +1,30 @@
+// Last updated: 13/08/2026, 09:23:55
+class Solution {
+    public boolean isValid(String s) {
+        
+        Stack<Character> st = new Stack<>();
+        for(char ch : s.toCharArray()){
+            if(ch == '(' || ch =='[' || ch == '{'){
+                st.push(ch);
+            }else{
+                if(st.isEmpty()){
+                    return false;
+                }
+            
+            
+                char top = st.pop();
+                if(ch == ')' && top !='('){
+                    return false;
+                }
+                if(ch == ']' && top !='['){
+                    return false;
+                }
+                if(ch == '}' && top != '{'){
+                    return false;
+                }
+            }
+            
+        }
+        return st.isEmpty();
+    }
+}
