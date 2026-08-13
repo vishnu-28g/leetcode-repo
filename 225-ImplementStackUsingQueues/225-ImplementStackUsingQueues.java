a7e35b3967ea16d44fe9bee077c6bca9c5978116
vishnu-28g <vishnuvishnu50097@gmail.com>
2026-08-13 09:19:25 +0530

    Create 225-ImplementStackUsingQueues

---
+++ 225-ImplementStackUsingQueues/225-ImplementStackUsingQueues.java
@@ -0,0 +1,25 @@
+// Last updated: 13/08/2026, 09:19:22
+class MyStack {
+    private Queue<Integer> q;
+    public MyStack() {
+        q= new LinkedList<>();
+    }
+    
+    public void push(int x) {
+        q.add(x);
+        for(int i=0;i<q.size()-1;i++)
+        q.add(q.poll());
+    }
+    
+    public int pop() {
+        return q.poll();
+    }
+    
+    public int top() {
+        return q.peek();
+    }
+    
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
